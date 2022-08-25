e6f142da0bf93629f9ea5aff33855b1f38bc2dea
JoJoPnz <act21078@gmail.com>
2022-08-25 15:03:45 +0700

    lec03-1 add calculator score to grade

--- lec03-1/app/controllers/main_controller.rb
+++ lec03-1/app/controllers/main_controller.rb
@@ -8,12 +8,35 @@ class MainController < ApplicationController
   end
 
   def test3
+    # render body: "hello"
     @x1 = params[:abc]
     @x2 = params[:def]
-    # render body: "hello"
   end
 
   def test4
     render raw: "hello"
   end
+
+  def calculator
+    @score = params[:grade].to_i
+    @grade = "X"
+    if @score < 50
+      @grade = "F"
+    elsif @score < 55
+      @grade = "D"
+    elsif @score < 60
+      @grade = "D+"
+    elsif @score < 65
+      @grade = "C"
+    elsif @score < 70
+      @grade = "C+"
+    elsif @score < 75
+      @grade = "B"
+    elsif @score < 80
+      @grade = "B+"
+    elsif @score >= 80
+      @grade = "A"
+    end
+  end
+
 end
--- lec03-1/config/routes.rb
+++ lec03-1/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'test2' , to: 'main#test2'
   get 'test3' , to: 'main#test3'
   get 'test4' , to: 'main#test4'
+  get 'main/calculator', to: 'main#calculator'
   # Defines the root path route ("/")
   # root "articles#index"
 end
