725dbd0eac4f3cc109467ac979b80124f4b00449
JoJoPnz <act21078@gmail.com>
2022-09-11 21:50:20 +0700

    hw03 Edit form style

--- hw03/app/controllers/main_controller.rb
+++ hw03/app/controllers/main_controller.rb
@@ -1,7 +1,12 @@
 class MainController < ApplicationController
   def test
     @amount = params[:amount].to_i
+    @subject_array = params[:subject]
+    @score_array = params[:score]
     @line_empty = params[:lineEmpty]
+    if(!@line_empty.nil?)
+      @line_empty = @line_empty.map{ |e| e.to_i}
+    end
   end
 
   def test2
@@ -11,16 +16,21 @@ class MainController < ApplicationController
     @line_empty = []
     @subject_array = params[:subject]
     @score_array = params[:score]
+    @subject_array_query = ""
+    @score_array_query = ""
+    @line_empty_query = ""
     for i in 1..@subject_array.length()
-      if(@subject_array[i-1] == "" || @score_array[i-1] == "")
+      if(@subject_array[i-1].blank? || @score_array[i-1] == "")
         @line_empty.push(i)
+        @line_empty_query += "&lineEmpty[]=#{i}"
       end
+      @subject_array_query += "&subject[]=#{@subject_array[i-1]}"
+      @score_array_query += "&score[]=#{@score_array[i-1]}"
     end
-
-    # render plain: @line_empty
     # if empty line => redirect to fill table
     if(@line_empty.length() > 0)
-      redirect_to("/main/test/?amount=#{@subject_array.length()}&lineEmpty=#{@line_empty}")
+      # create query params array for lineEmpty var.
+      redirect_to("/main/test/?amount=#{@subject_array.length()}" + @subject_array_query + @score_array_query + @line_empty_query)
     end
 
     # calculate the answer
